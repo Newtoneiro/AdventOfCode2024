edbc58a0227d2b9732cd916588f402eca9a8a77e
Newtoneiro <latosek.bar@gmail.com>
2024-12-21 15:12:51 +0100

    add caching

--- 21/main.py
+++ 21/main.py
@@ -1,30 +1,30 @@
 import os
+from collections import deque
+from functools import lru_cache
 
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, "input.txt")
 
-KEYPAD = [
-    ['7', '8', '9'],
-    ['4', '5', '6'],
-    ['1', '2', '3'],
-    ['', '0', 'A'],
-]
-KEYPAD_DICT = {
-    key: [x, y] for y, row in enumerate(KEYPAD)
-    for x, key in enumerate(row)
-}
-INIT_KEYPAD_POS = [2, 3]
+KEYPAD = (
+    ('7', '8', '9'),
+    ('4', '5', '6'),
+    ('1', '2', '3'),
+    ('', '0', 'A'),
+)
+INIT_KEYPAD_POS = (2, 3)
 
-CONTROLLER = [
-    ['', '^', 'A'],
-    ['<', 'v', '>'],
-]
-CONTROLLER_DICT = {
-    key: [x, y] for y, row in enumerate(CONTROLLER)
-    for x, key in enumerate(row)
-}
-INIT_CONTROLLER_POS = [2, 0]
+CONTROLLER = (
+    ('', '^', 'A'),
+    ('<', 'v', '>'),
+)
+INIT_CONTROLLER_POS = (2, 0)
 
+MOVES = {
+    "<": (-1, 0),
+    ">": (1, 0),
+    "^": (0, -1),
+    "v": (0, 1),
+}
 
 def get_codes():
     codes = []
@@ -33,34 +33,55 @@ def get_codes():
             codes.append([c for c in line.strip()])
     return codes
 
+@lru_cache(maxsize=None)
+def get_move(grid, pos, ch_out):
+    board_dict =  {
+        key: [x, y] for y, row in enumerate(grid)
+        for x, key in enumerate(row)
+    }
+    next_out = deque()
+    gap = board_dict['']
+    next_pos = board_dict[ch_out]
+    if next_pos[1] != pos[1]:
+        y_movement_key = "v" if next_pos[1] > pos[1] else "^"
+        for i in range(abs(next_pos[1] - pos[1])):
+            next_out.append(y_movement_key)
+    if next_pos[0] != pos[0]:
+        x_movement_key = ">" if next_pos[0] > pos[0] else "<"
+        for i in range(abs(next_pos[0] - pos[0])):
+            next_out.append(x_movement_key)
+        
+    nex_out_fixed = []
+    cur_pos = pos
+    while len(next_out):
+        ch = next_out.popleft()
+        move = MOVES[ch]
+        cur_pos = [
+            cur_pos[0] + move[0],
+            cur_pos[1] + move[1]
+        ]
+        if cur_pos == gap:
+            gap_avoid_ch = next_out.pop()
+            gap_avoid_move = MOVES[gap_avoid_ch]
+            cur_pos = [
+                cur_pos[0] + gap_avoid_move[0],
+                cur_pos[1] + gap_avoid_move[1]
+            ]
+            nex_out_fixed.append(gap_avoid_ch)
+            nex_out_fixed.append(ch)
+        else:
+            nex_out_fixed.append(ch)
+    
+    nex_out_fixed.append("A")
+    return nex_out_fixed, (next_pos[0], next_pos[1])
 
-def get_movements(board_dict, initial_pos, sequence):
+def get_movements(board, initial_pos, sequence):
     out = []
     pos = initial_pos
-    gap = board_dict['']
     for ch in sequence:
-        next_out = []
-        next_pos = board_dict[ch]
-
-        swap = False
-        if next_pos[1] != pos[1]:
-            y_movement_key = "v" if next_pos[1] > pos[1] else "^"
-            for i in range(abs(next_pos[1] - pos[1])):
-                next_out.append(y_movement_key)
-                dir = 1 if y_movement_key == "v" else -1
-                if pos[1] + dir * (i+1) == gap[1]:
-                    swap = True
-        if next_pos[0] != pos[0]:
-            x_movement_key = ">" if next_pos[0] > pos[0] else "<"
-            for i in range(abs(next_pos[0] - pos[0])):
-                if not swap:
-                    next_out.append(x_movement_key)
-                else:
-                    next_out.insert(0, x_movement_key)
-        next_out.append("A")
-
-        pos = next_pos
+        next_out, next_pos = get_move(board, pos, ch)
         out += next_out
+        pos = next_pos
     return out
 
 
@@ -76,9 +97,9 @@ def main_one():
     total = 0
     codes = get_codes()
     for code in codes:
-        first_robot = get_movements(KEYPAD_DICT, INIT_KEYPAD_POS, code)
-        second_robot = get_movements(CONTROLLER_DICT, INIT_CONTROLLER_POS, first_robot)
-        my_input = get_movements(CONTROLLER_DICT, INIT_CONTROLLER_POS, second_robot)
+        first_robot = get_movements(KEYPAD, INIT_KEYPAD_POS, code)
+        second_robot = get_movements(CONTROLLER, INIT_CONTROLLER_POS, first_robot)
+        my_input = get_movements(CONTROLLER, INIT_CONTROLLER_POS, second_robot)
         total += len(my_input) * cast_to_int(code)
     return total
 
@@ -87,11 +108,12 @@ def main_two():
     total = 0
     codes = get_codes()
     for code in codes:
-        first_robot = get_movements(KEYPAD_DICT, INIT_KEYPAD_POS, code)
+        first_robot = get_movements(KEYPAD, INIT_KEYPAD_POS, code)
         for i in range(25):
-            second_robot = get_movements(CONTROLLER_DICT, INIT_CONTROLLER_POS, first_robot)
+            print(i)
+            second_robot = get_movements(CONTROLLER, INIT_CONTROLLER_POS, first_robot)
             first_robot = second_robot
-        my_input = get_movements(CONTROLLER_DICT, INIT_CONTROLLER_POS, second_robot)
+        my_input = get_movements(CONTROLLER, INIT_CONTROLLER_POS, second_robot)
         total += len(my_input) * cast_to_int(code)
     return total
 
